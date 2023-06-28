514aa8da791ab7fad09cf5febb8b75e27e75b7c3
Kowshik Kuri <ratulkuri@gmail.com>
2023-06-29 03:22:53 +0600

    Removes junk code from app.js

--- src/app.js
+++ src/app.js
@@ -26,19 +26,6 @@ const init = () => {
     rederProductList(".product-list");
     InitCart("#cart-menu");
     productModalInit();
-    // Toast.success({message: "This is success!", autoHide: true });
-    // Toast.error({message: "This is error!", autoHide: false });
-    // Toast.warning({message: "This is warning!", autoHide: false });
-
-    let prod = Products;
-
-    let a = Products.map((el) => {
-        return el.images
-    })
-
-    console.log(prod.length);
-
-
 }
 
 init();
